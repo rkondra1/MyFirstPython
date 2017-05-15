f7878c7675e5fe6b2b5f1a25800948f9ce176064
rkondra1 <rajesh_kondra@intuit.com>
2017-05-14 23:45:19 -0700

    Update

--- Hello/__init__.py
+++ Hello/__init__.py
@@ -1,5 +1,16 @@
+# List Exmaple
 x = [1,2,3,4]
 print(x[1])
 
+#Distionary exmaple
 y={"Name":"Rajesh", "email":"rajeshcse515@gmail.com"}
 print(y["Name"])
+
+#Truple example
+
+point = (1,2)
+print(point[0])
+
+# Functions eg
+
+print("Length" , len("Hello")).
